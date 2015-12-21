41dd70828cf5c0074112c2bce63594ea3d17e84d
Tyler <mistaken3lf@gmail.com>
2015-12-21 00:45:39 +0000

    Switched to semantic ui

---
+++ client/components/app.jsx
@@ -0,0 +1,9 @@
+App = React.createClass({
+  render() {
+    return (
+      <div className="ui active inverted dimmer">
+        <div className="ui text loader">Loading</div>
+      </div>
+    )
+  }
+});
