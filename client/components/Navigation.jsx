fe808c221016da6287f6e0e38f8812daff16e5ca
Tyler <mistaken3lf@gmail.com>
2016-01-08 15:28:34 +0000

    Reset project'

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,13 +1,7 @@
 Navigation = React.createClass({
   render() {
     return (
-      <nav>
-        <ul id="slide-out" className="side-nav fixed">
-          <li><a href="#!">First Sidebar Link</a></li>
-          <li><a href="#!">Second Sidebar Link</a></li>
-        </ul>
-        <a href="#" data-activates="slide-out" className="button-collapse"><i className="mdi-navigation-menu"></i></a>
-      </nav>
+      <h1>Navigation</h1>
     );
   }
 })
